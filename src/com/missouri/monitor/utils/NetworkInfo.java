925fff29d48ccfa90acd86e2f7ea47d6b26b6f9f
Ricky <rs79c@mail.missouri.edu>
2014-11-03 15:32:41 -0600

    network traffic

---
+++ src/com/missouri/monitor/utils/NetworkInfo.java
@@ -0,0 +1,68 @@
+package com.missouri.monitor.utils;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import com.missouri.monitor.logger.Logger;
+
+/**
+ * information of network traffic
+ *
+ */
+public class NetworkInfo {
+
+	private final static Logger log = Logger.getLogger(NetworkInfo.class);
+
+	private String uid;
+
+	public NetworkInfo(String uid) {
+		this.uid = uid;
+	}
+
+	/**
+	 * get total network traffic, which is the sum of upload and download
+	 * traffic.
+	 *
+	 * @return total traffic include received and send traffic
+	 */
+	public long getNetworkInfo() {
+		log.i("get traffic information");
+		RandomAccessFile rafRcv = null, rafSnd = null;
+		String rcvPath = "/proc/uid_stat/" + uid + "/tcp_rcv";
+		String sndPath = "/proc/uid_stat/" + uid + "/tcp_snd";
+		long rcvTraffic = -1;
+		long sndTraffic = -1;
+		try {
+			rafRcv = new RandomAccessFile(rcvPath, "r");
+			rafSnd = new RandomAccessFile(sndPath, "r");
+			rcvTraffic = Long.parseLong(rafRcv.readLine());
+			sndTraffic = Long.parseLong(rafSnd.readLine());
+		} catch (FileNotFoundException e) {
+			rcvTraffic = -1;
+			sndTraffic = -1;
+		} catch (NumberFormatException e) {
+			log.e( "NumberFormatException: " + e.getMessage());
+			e.printStackTrace();
+		} catch (IOException e) {
+			log.e("IOException: " + e.getMessage());
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rafRcv != null) {
+					rafRcv.close();
+				}
+				if (rafSnd != null) {
+					rafSnd.close();
+				}
+			} catch (IOException e) {
+				log.i("close randomAccessFile exception: " + e.getMessage());
+			}
+		}
+		if (rcvTraffic == -1 || sndTraffic == -1) {
+			return -1;
+		} else {
+			return rcvTraffic + sndTraffic;
+		}
+	}
+}
