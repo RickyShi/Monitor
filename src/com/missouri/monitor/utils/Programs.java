d21692e357668b17236217d518c6db17c6b9bbcf
Ricky <rs79c@mail.missouri.edu>
2014-11-02 21:46:16 -0600

    first commit

---
+++ src/com/missouri/monitor/utils/Programs.java
@@ -0,0 +1,65 @@
+package com.missouri.monitor.utils;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * details of installed processes ,including
+ * icon,packagename,pid,uid,processname
+ * 
+ * @author Ricky
+ * 
+ */
+public class Programs implements Comparable<Programs> {
+	private Drawable icon;
+	private String processName;
+	private String packageName;
+	private int pid;
+	private int uid;
+
+	public int getUid() {
+		return uid;
+	}
+
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+
+	public Drawable getIcon() {
+		return icon;
+	}
+
+	public void setIcon(Drawable icon) {
+		this.icon = icon;
+	}
+
+	public String getProcessName() {
+		return processName;
+	}
+
+	public void setProcessName(String processName) {
+
+		this.processName = processName;
+	}
+
+	public String getPackageName() {
+		return packageName;
+	}
+
+	public void setPackageName(String packageName) {
+		this.packageName = packageName;
+	}
+
+	public int getPid() {
+		return pid;
+	}
+
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+		@Override
+	public int compareTo(Programs another) {
+		// TODO Auto-generated method stub
+		return (this.getProcessName().compareTo(another.getProcessName()));
+		}
+}
